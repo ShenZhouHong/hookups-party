b62532f6cb9da6a2e0c0de76cfc753bd166ccc53
peterpacz1 <szhong@uwcchina.org>
2017-02-19 15:57:40 +0800

    Added slide for activities

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -4,3 +4,10 @@ $('.is-gender').on('click', function(event) {
         scrollTop: $("#preferences").offset().top
     }, 1000);
 });
+
+$('.want-gender').on('click', function(event) {
+    event.preventDefault(); // To prevent following the link (optional)
+    $('html, body').animate({
+        scrollTop: $("#activities").offset().top
+    }, 1000);
+});
