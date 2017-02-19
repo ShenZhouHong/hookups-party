5de41624eaa8ce3552c637043da6f036821ff878
peterpacz1 <szhong@uwcchina.org>
2017-02-20 02:58:07 +0800

    Created information page scaffolding

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hookups @ CSC', script: 'scripts.js' });
+  res.render('index', {
+      title: 'Hookups @ CSC',
+      script: 'scripts.js',
+      style: 'style.css'
+  });
 });
 
 /* GET chat page. */
 router.get('/chat', function(req, res, next) {
-  res.render('chat', { title: 'Hooking Up ;)', script: 'chat.js' });
+  res.render('chat', {
+      title: 'Hooking Up ;)',
+      script: 'chat.js',
+      style: 'chat.css'
+  });
+});
+
+/* GET information page. */
+router.get('/information', function(req, res, next) {
+  res.render('information', {
+      title: 'Information',
+      script: 'information.js',
+      style: 'information.css'
+  });
 });
 
 module.exports = router;
