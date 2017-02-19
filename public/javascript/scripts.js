aa62018e8fe3716e29a2753d6a05af3b9c7fb567
peterpacz1 <szhong@uwcchina.org>
2017-02-19 17:45:14 +0800

    Added alert functionality to gender selection

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,12 +1,32 @@
+firstGenderSelect = true;
+
 $('.is-gender').on('click', function(event) {
     event.preventDefault(); // To prevent following the link (optional)
-    $('html, body').animate({
-        scrollTop: $("#preferences").offset().top
-    }, 1000);
+
+    // Locks in this button
+    $('.is-gender').removeClass('active');
+    $(this).addClass('active');
+
+    // Prevents annoying scroll behavior on second choice.
+    if (firstGenderSelect == true) {
+        $('html, body').animate({
+            scrollTop: $("#preferences").offset().top
+        }, 1000);
+        firstGenderSelect = false;
+    }
+    else {
+        // Tells user to advance on to the next slide
+        console.log("Else'ed");
+        $('#alert-placeholder-here').html('<div class="alert alert-success alert-dismissible bottom" role="alert" id="continue-message"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Success!</strong> Your gender has been changed ;)</div>');
+    }
 });
 
 $('.want-gender').on('click', function(event) {
     event.preventDefault(); // To prevent following the link (optional)
+
+    // Locks in this button
+    $(this).addClass('active');
+
     $('html, body').animate({
         scrollTop: $("#activities").offset().top
     }, 1000);
