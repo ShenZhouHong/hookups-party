b6c9729e979a0df48b55b659bc0443aac9adad46
peterpacz1 <szhong@uwcchina.org>
2017-02-23 02:28:36 +0800

    Implemented prototype code for bubble styling. Extremely unoptimised, will change.

--- resources/sources/js/chat.js
+++ resources/sources/js/chat.js
@@ -15,6 +15,62 @@ var DisplayChat = function() {
     }, 1000);
 };
 
+// Styles the chat bubbles to give them the correct borders
+/*
+    This is some shit-ass spaghetti code. Please forgive me. I will make it
+    better later. It's so shit it is called every time a message is sent.
+    Unforgivable.
+*/
+var StyleBubble = function() {
+    // for each self message on the page
+    $( "li.self-message" ).each(function() {
+        // If the bubble is the first in the chat
+        if ($(this).is(':first-child') && $(this).next().hasClass('self-message')){
+            $(this).addClass('self-message-top');
+        }
+        // If the bubble is the first of it's siblings
+        else if ($(this).prev().hasClass('other-message') && $(this).next().hasClass('self-message')){
+            $(this).addClass('self-message-top');
+        }
+        // If the bubble is in between it's siblings
+        else if ($(this).prev().hasClass('self-message') && $(this).next().hasClass('self-message')){
+            $(this).addClass('self-message-med');
+        }
+        // If the bubble is the last of it's siblings
+        else if ($(this).prev().hasClass('self-message') && $(this).next().hasClass('other-message')){
+            $(this).addClass('self-message-low');
+        }
+        // If the bubble is the last in the chat
+        else if ($(this).prev().hasClass('self-message') && $(this).is(':last-child')){
+            $(this).addClass('self-message-low');
+        }
+    });
+
+    // for each other message on the page
+    $( "li.other-message" ).each(function() {
+        // If the bubble is the first in the chat
+        if ($(this).is(':first-child') && $(this).next().hasClass('other-message')){
+            $(this).addClass('other-message-top');
+        }
+        // If the bubble is the first of it's siblings
+        else if ($(this).prev().hasClass('self-message') && $(this).next().hasClass('other-message')){
+            $(this).addClass('other-message-top');
+        }
+        // If the bubble is in between it's siblings
+        else if ($(this).prev().hasClass('other-message') && $(this).next().hasClass('other-message')){
+            $(this).addClass('other-message-med');
+        }
+        // If the bubble is the last of it's siblings
+        else if ($(this).prev().hasClass('other-message') && $(this).next().hasClass('self-message')){
+            $(this).addClass('other-message-low');
+        }
+        // If the bubble is the last in the chat
+        else if ($(this).prev().hasClass('other-message') && $(this).is(':last-child')){
+            $(this).addClass('other-message-low');
+        }
+     });
+};
+
 (function () {
     var userPreferences = {
         selfGender: "male",
@@ -64,6 +120,7 @@ var DisplayChat = function() {
 
             // Appends message
             $("#chat-messages").append(elem);
+            StyleBubble();
 
         });
 
