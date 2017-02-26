47446eef117139e8cdeb24f87f534f692bbd3659
peterpacz1 <szhong@uwcchina.org>
2017-02-26 17:54:37 +0800

    Modularised Grunt.js to use proper deployment targets

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,22 +11,35 @@ module.exports = function(grunt) {
             by per 'target' configuration. Targets can have abritary names, and
             I have organized them by their source hbs names
         */
+
         // Minifies CSS stylesheets
         cssmin: {
-            /*
-                IMPORTANT: Always include fonts.css as the 2nd stylesheet,
-                immediately after bootstrap.css. It is required by the logo
-                and subtitles.
-            */
+
             // Global options. Per target options can also be defined.
             options: {
-                sourceMap: true,
                 mergeIntoShorthands: false,
                 roundingPrecision: -1
             },
 
-            // Begin list of targets
-            index: {
+            /*
+                Begin list of targets:
+
+                IMPORTANT: Always include fonts.css as the 2nd stylesheet,
+                immediately after bootstrap.css. It is required by the logo
+                and subtitles.
+            */
+
+            /*
+                Production target minifies and combines all stylsheets for fast
+                content delivery and minimal file size. Sourcemaps are ommited
+                from a production setup, use development for sourcemaps.
+            */
+            production: {
+                // Per-target options
+                options: {
+                    sourceMap: false,
+                },
+
                 // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
                 files: {
                     'public/css/index.min.css': [
@@ -36,22 +49,12 @@ module.exports = function(grunt) {
                         'resources/sources/css/index.css',
                         'resources/sources/css/chat-ui.css'
                     ],
-                }
-            },
-            information: {
-                // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
-                files: {
                     'public/css/information.min.css': [
                         'node_modules/bootstrap/dist/css/bootstrap.css',
                         'resources/sources/css/fonts.css',
                         'resources/sources/css/common.css',
                         'resources/sources/css/information.css'
                     ],
-                }
-            },
-            error : {
-                // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
-                files: {
                     'public/css/error.min.css': [
                         'node_modules/bootstrap/dist/css/bootstrap.css',
                         'resources/sources/css/fonts.css',
@@ -59,21 +62,63 @@ module.exports = function(grunt) {
                         'resources/sources/css/error.css'
                     ],
                 }
-            }
+            },
+
+            /*
+                Development target minifies and combines stylesheets, for a
+                production-equivelent speed (useful for benchmarks) while
+                preserving sourcemaps for easier debugging
+            */
+            development: {
+                // Per-target options
+                options: {
+                    sourceMap: true,
+                },
+
+                // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
+                files: {
+                    'public/css/index.min.css': [
+                        'node_modules/bootstrap/dist/css/bootstrap.css',
+                        'resources/sources/css/fonts.css',
+                        'resources/sources/css/common.css',
+                        'resources/sources/css/index.css',
+                        'resources/sources/css/chat-ui.css'
+                    ],
+                    'public/css/information.min.css': [
+                        'node_modules/bootstrap/dist/css/bootstrap.css',
+                        'resources/sources/css/fonts.css',
+                        'resources/sources/css/common.css',
+                        'resources/sources/css/information.css'
+                    ],
+                    'public/css/error.min.css': [
+                        'node_modules/bootstrap/dist/css/bootstrap.css',
+                        'resources/sources/css/fonts.css',
+                        'resources/sources/css/common.css',
+                        'resources/sources/css/error.css'
+                    ],
+                }
+            },
         },
 
         // Minifies Javascript files
         uglify: {
             // Global options. Per target options can also be defined.
             options: {
-                // Make development a joy :)
-                sourceMap: true,
-                sourceMapIncludeSources: true,
                 report: 'gzip'
             },
 
             // Begin list of targets
-            index: {
+            /*
+                Production target combines, minifies, and mangles all the source
+                files in order to reduce filesize and improve speed. sourcemaps
+                are not included to save some additional bytes.
+            */
+            production: {
+                options: {
+                    sourceMap: false,
+                    sourceMapIncludeSources: false,
+                    mangle: true,
+                },
                 // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
                 files: {
                     'public/js/index.min.js': [
@@ -83,28 +128,51 @@ module.exports = function(grunt) {
                             'resources/sources/js/index.js',
                             'resources/sources/js/chat.js',
                             'resources/sources/js/chat-ui.js'
-                        ],
-                }
-            },
-            information: {
-                // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
-                files: {
+                    ],
                     'public/js/information.min.js': [
                             'node_modules/jquery/dist/jquery.js',
                             'node_modules/bootstrap/dist/js/bootstrap.js',
                             'resources/sources/js/information.js',
-                        ],
-                }
-            },
-            error: {
-                // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
-                files: {
+                    ],
                     'public/js/error.min.js': [
                             'node_modules/jquery/dist/jquery.js',
                             'node_modules/bootstrap/dist/js/bootstrap.js',
-                        ],
+                    ],
                 }
-            }
+            },
+
+            /*
+                Developmet target is similar to production target, but includes
+                sourcemaps for the minimized resources in order to faciliate
+                debugging. Just ever so slightly larger.
+            */
+            development: {
+                options: {
+                    sourceMap: true,
+                    sourceMapIncludeSources: true,
+                    mangle: true,
+                },
+                // Syntax 'path/min.css': ['path/src1.css', 'path/src2.css']
+                files: {
+                    'public/js/index.min.js': [
+                            'node_modules/jquery/dist/jquery.js',
+                            'node_modules/socket.io-client/dist/socket.io.js',
+                            'node_modules/bootstrap/dist/js/bootstrap.js',
+                            'resources/sources/js/index.js',
+                            'resources/sources/js/chat.js',
+                            'resources/sources/js/chat-ui.js'
+                    ],
+                    'public/js/information.min.js': [
+                            'node_modules/jquery/dist/jquery.js',
+                            'node_modules/bootstrap/dist/js/bootstrap.js',
+                            'resources/sources/js/information.js',
+                    ],
+                    'public/js/error.min.js': [
+                            'node_modules/jquery/dist/jquery.js',
+                            'node_modules/bootstrap/dist/js/bootstrap.js',
+                    ],
+                }
+            },
         },
 
         // Copies fonts and other files
@@ -126,7 +194,8 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    // Default task.
-    grunt.registerTask('default', ['cssmin', 'uglify', 'copy']);
+    // Register seperate production versus development-based tasks
+    grunt.registerTask('development', ['env:development', 'cssmin:development', 'uglify:development', 'copy']);
+    grunt.registerTask('production', ['env:production', 'cssmin:production', 'uglify:production', 'copy']);
 
 };
