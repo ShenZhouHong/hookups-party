50a05a698ce3735ee509778829ed9a1d269e99c0
pinusc <giuseppe.stelluto@gmail.com>
2017-04-28 21:26:09 +0800

    Animate timer + fix fonts

--- resources/sources/js/wait.js
+++ resources/sources/js/wait.js
@@ -1,7 +1,19 @@
 /* Makes sure that the DOM is only manipulated when ready */
 $(function () {
     "use strict";
-
-    console.log( "ready!" );
-
+    var a = function() {
+        var date = new Date();
+        var second = parseInt(date.toLocaleString('en-US', {second: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' }));
+        var minute = parseInt(date.toLocaleString('en-US', {minute: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' }));
+        var hour = parseInt(date.toLocaleString('en-US', {hour: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' }));
+        var remaining = (((23 - hour) * 60) - minute) * 60 - second;
+        console.log(remaining);
+        if (! remaining) {
+            $(location).attr('href', '/');
+        }
+        $("#heartbeats").text(remaining);
+    };
+    a();
+    $("#heartbeats").css("opacity", 1);
+    window.setInterval(a, 1000);
 });
