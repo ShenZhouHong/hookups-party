74f002df8127305cb765047726987321a507cd9c
pinusc <giuseppe.stelluto@gmail.com>
2017-04-28 20:22:20 +0800

    Implement "blockme" button

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function (req, res, next) {
+    if (req.cookies.blocked === "blocked") {
+        res.redirect("http://www.solidhookups.com/");
+    } else {
+        next()
+    }
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', {
-        title: 'Hookups @ CSC',
-        // Included resources
-        js: ['index.min.js'],
-        css: ['index.min.css']
-    });
+    var date = new Date();
+    var minute = parseInt(date.toLocaleString('en-US', {minute: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' }));
+    var hour = parseInt(date.toLocaleString('en-US', {hour: '2-digit', hour12: false, timeZone: 'Asia/Shanghai' }));
+    if (process.env.DEV || (hour === 22 && minute <= 30)) {
+        res.render('index', {
+            title: 'Hookups @ CSC',
+            // Included resources
+            js: ['index.min.js'],
+            css: ['index.min.css']
+        });
+    } else {
+        res.render('wait', {
+            title: 'Wait for Hookups @ CSC',
+            // Included resources
+            js: ['wait.min.js'],
+            css: ['wait.min.css']
+        });
+    }
 });
 
 /* GET information page. */
@@ -29,6 +49,11 @@ router.get('/session', function(req, res, next) {
       js: ['session.min.js'],
       css: ['session.min.css']
     });
+});
+
+router.get('/blockme', function(req,res,next){
+    res.cookies('blocked', 'blocked', {maxAge: -1});
+    res.redirect("/");
 });
 
 module.exports = router;
