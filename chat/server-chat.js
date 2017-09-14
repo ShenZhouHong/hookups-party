cac2f6d8922d2e09cd538610e86757497818b89a
ShenZhouHong <shen@hong.io>
2017-09-14 16:39:02 +0800

    Added fancy logging

--- chat/server-chat.js
+++ chat/server-chat.js
@@ -1,5 +1,30 @@
 module.exports = function(server, app) {
+    var io      = require('socket.io')(server);
+    const chalk = require('chalk');
 
+    io.on('connection', function(socket){
+        console.log(
+            chalk.bold.bgBlue("INFO:") + " " +
+            "A client has connected to socket " + chalk.bold.underline(socket.id) + ": " +
+            "\n      Remote IP : " + chalk.bold.underline(socket.request.connection.remoteAddress) +
+            "\n      User-Agent: " + chalk.bold.underline(socket.request.headers['user-agent'])
+
+        );
+
+        socket.on('requestQueue', function(msg) {
+            console.log(
+                "\n      UserPrefs :" + msg
+            );
+        });
+
+        socket.on('disconnect', function(){
+            console.log(
+                chalk.bold.bgBlue("INFO:") + " " +
+                "A client has disconnected from socket " +
+                chalk.bold.underline(socket.id)
+            );
+        });
+    });
 
     return module;
 };
--- resources/sources/js/client-chat.js
+++ resources/sources/js/client-chat.js
@@ -1,3 +1,6 @@
 var requestQueue = function(userPreferences) {
-
+    // Establishes a connection with the server
+    var socket = io({forceNew: true});
+    socket.emit("requestQueue", userPreferences);
+    
 };
